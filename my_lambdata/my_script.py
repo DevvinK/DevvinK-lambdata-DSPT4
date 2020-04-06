0422ef68065689da0cb7b85672c6ac27b9de1940
DevvinK <devvink@gmail.com>
2020-04-05 20:36:50 -0600

    Import fuction from mod

--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,12 +1,13 @@
 # my_lambdata/my_script.py
 
 import pandas
-# from my_lambdata.my_mod import enlarge
+from my_lambdata.my_mod import enlarge
 
 print("Happy Tuesday night.")
 
 df = pandas.DataFrame({"x": [1,2,3], 'y':[4,5,6]})
 print(df.head())
 
-# x=5
-# print('Enlarged', x, 'To', enlarge(x))
+x=5
+print('Enlarged', x, 'To', enlarge(x))
+
