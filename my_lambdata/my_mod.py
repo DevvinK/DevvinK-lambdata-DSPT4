9badbb54b0186b89d415ece507fcf87f653de881
DevvinK <devvink@gmail.com>
2020-04-05 20:43:41 -0600

    Demonstrate import nuances

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -2,3 +2,10 @@
 
 def enlarge(n):
     return n * 100
+
+if __name__ == "__main__":
+        print("Junk")
+        print("Global scope")
+
+        y = float(input("Please input a number to enlarge: "))
+        print(enlarge(y))
